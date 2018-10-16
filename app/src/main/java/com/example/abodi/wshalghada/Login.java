093a43281f64f9420a514a5d722a4f9f05158024
safaa <safaalsugair@gmail.com>
2018-10-16 18:23:38 +0300

    Login

--- app/src/main/java/com/example/abodi/wshalghada/Login.java
+++ app/src/main/java/com/example/abodi/wshalghada/Login.java
@@ -1,27 +1,24 @@
 package com.example.abodi.wshalghada;
-
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Bundle;
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import java.sql.*;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import android.widget.Button;
+import android.widget.Toast;
 public class Login extends AppCompatActivity {
 
+    SharedPreferences sp;
     ResultSet resultSet = null;
-    String userNameDB = "root";
-    String passwordDB = "";
+    String userNameDB ;
+    String passwordDB;
     static final String username="Username";
+
     static SharedPreferences getSharedPreferences(Context context){
         return PreferenceManager.getDefaultSharedPreferences(context);
     }
@@ -35,55 +32,76 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-         login();
+        sp = getSharedPreferences("login",Context.MODE_PRIVATE);
+        if(sp.getBoolean("logged",false)){
+            Intent intent = new Intent(Login.this, MainActivity.class);
+            startActivity(intent);
+        }
+        login();
 
     }
-
     private void login() {
 
-        Button LoginButton =(Button) findViewById(R.id.enter3);
+        Button LoginButton = findViewById(R.id.enter3);
         LoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText UserName=(EditText) findViewById(R.id.username1);
-                EditText Password=(EditText) findViewById(R.id.password1);
-                String userName= UserName.getText().toString();
-                String password= Password.getText().toString();
+                EditText UserName =  findViewById(R.id.username1);
+                EditText Password =  findViewById(R.id.password1);
+                String userName = UserName.getText().toString();
+                String password = Password.getText().toString();
+                if (!(password.equals("") || userName.equals(""))) {
 
-                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-                        .permitAll().build();
-                StrictMode.setThreadPolicy(policy);
-                try
-                {
-                    Class.forName(DBConnection.urlstring);
-                    Connection con= DriverManager.getConnection(DBConnection.urlstring, DBConnection.username, DBConnection.password); //establishing connection
-                   // StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-                        //    .permitAll().build();
-                    //StrictMode.setThreadPolicy(policy);
-                    // con = DriverManager.getConnection("jdbc:mysql://192.168.100.14/wshalghada","root",null);
-                    Statement statement  = con.createStatement(); //Statement is used to write queries. Read more about it.
-                    resultSet = statement.executeQuery("select Username,Password from user"); //Here table name is users and userName,password are columns. fetching all the records and storing in a resultSet.
-                    while(resultSet.next()) // Until next row is present otherwise it return false
-                    {
-                        userNameDB = resultSet.getString("Username"); //fetch the values present in database
-                        passwordDB = resultSet.getString("Password");
-                        if(userName.equals(userNameDB) && password.equals(passwordDB))
+                    try {
+                        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                                .permitAll().build();
+                        StrictMode.setThreadPolicy(policy);
+                         Class.forName("com.mysql.jdbc.Driver");
+
+                        Connection con = DriverManager.getConnection(DBConnection.urlstring, DBConnection.username, DBConnection.password);
+
+
+                        Statement statement = con.createStatement(); //Statement is used to write queries. Read more about it.
+                        resultSet = statement.executeQuery("select Username,Password from user"); //Here table name is users and userName,password are columns. fetching all the records and storing in a resultSet.
+                        while (resultSet.next()) // Until next row is present otherwise it return false
                         {
-
-                            Intent intent = new Intent(Login.this,MainActivity.class);
-                            startActivity(intent);
+                            userNameDB = resultSet.getString("Username"); //fetch the values present in database
+                            passwordDB = resultSet.getString("Password");
+                            if (userName.equals(userNameDB) && password.equals(passwordDB)) {
+                                final SharedPreferences.Editor editor = sp.edit();
+                                editor.putBoolean("logged",true).apply();
+                                editor.putString("username",userNameDB);
+                                editor.commit();
+                                Intent intent = new Intent(Login.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+                            else  Toast.makeText(Login.this, "اسم المستخدم او كلمة المرور خطأ",
+                                    Toast.LENGTH_LONG).show();
                         }
-                    }}
+                    } catch (SQLException e) {
+                        e.printStackTrace();} catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
 
-                catch(SQLException e)
-                {
-                    e.printStackTrace();
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
                 }
 
+                else   Toast.makeText(Login.this, "يجب تعبئية جميع الحقول المطلوبه",
+                        Toast.LENGTH_LONG).show();
             }
+
+
+
+
+
+
+
+
+
+
 
         });
     }
 }
+
+
+
