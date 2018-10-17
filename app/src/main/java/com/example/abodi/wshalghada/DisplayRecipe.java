c9f2da4892151881ec964c0dd8cb02aca144119e
Ghada <Ghadaj123@hotmail.com>
2018-10-17 18:14:56 +0300

    display recipe

--- app/src/main/java/com/example/abodi/wshalghada/DisplayRecipe.java
+++ app/src/main/java/com/example/abodi/wshalghada/DisplayRecipe.java
@@ -3,7 +3,8 @@ package com.example.abodi.wshalghada;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -35,6 +37,9 @@ public class DisplayRecipe extends AppCompatActivity {
     int numofF;
     SharedPreferences sp =getSharedPreferences("login", Context.MODE_PRIVATE);
     String userLogin = sp.getString("username",null);
+    private Blob blobimage;
+    private byte[] bytesimage;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,14 +64,15 @@ public class DisplayRecipe extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         Connection con;
-        Statement stmt;
+        Statement stmt, stmt1;
         String sql, sql1, sql2, sql3;
-        String IngredientList=null;
-        ResultSet resultSet, resultIngredients, resultContain, resultSet2;
+        String IngredientList="";
+        ResultSet resultSet, resultSet2;
         try {
             Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection(DBConnection.urlstring, DBConnection.username, DBConnection.password);
             stmt = con.createStatement();
+            stmt1 = con.createStatement();
 
             /////////////////RecipeID!!!!!!!!
             sql = "SELECT * FROM recipe WHERE RecipeID = '1' ";
@@ -76,40 +82,31 @@ public class DisplayRecipe extends AppCompatActivity {
                 recipeName.setText(resultSet.getString("RecipeName"));
                 userName.setText(resultSet.getString("Username"));
                 dateON.setText(resultSet.getString("Date"));
-                image.setImageURI(Uri.parse(resultSet.getString("Photo")));
+
+                blobimage= resultSet.getBlob("Photo");
+                bytesimage=(blobimage.getBytes(1,(int)blobimage.length()));
+                bitmap=BitmapFactory.decodeByteArray(bytesimage,0,bytesimage.length);
+                image.setImageBitmap(bitmap);
+
                 steps.setText(resultSet.getString("Instruction"));
                 serving.setText(resultSet.getString("NumOfServing"));
                 time.setText(resultSet.getString("CookingTime"));
                 numofF=resultSet.getInt("NumOfF");
             }
 
-            /*
             /////////////////RecipeID
             sql1 = "SELECT * FROM contain WHERE RecipeID = '1'";
-            resultContain = stmt.executeQuery(sql1);
-
+            ResultSet resultContain = stmt.executeQuery(sql1);
             while (resultContain.next()) {
-                IngredientList = IngredientList + (resultContain.getString("Number")+" ");
-                IngredientList = IngredientList + (resultContain.getString("Unit")+" ");
-                sql2 = "SELECT IngredientName FROM ingredient WHERE IngredientID ="+resultContain.getString("IngredientID");
-                resultIngredients = stmt.executeQuery(sql2);
-                IngredientList = IngredientList + (resultIngredients.getString("IngredientName")+"\n");
+                IngredientList = IngredientList.concat(resultContain.getString("Number")+" ");
+                IngredientList = IngredientList.concat(resultContain.getString("Unit")+" ");
+                sql2 = "SELECT IngredientName FROM ingredient WHERE IngredientID ='"+resultContain.getString("IngredientID") +"'";
+                ResultSet resultIngredients = stmt1.executeQuery(sql2);
+                while (resultIngredients.next()) {
+                    IngredientList = IngredientList.concat(resultIngredients.getString("IngredientName")+"\n");
+                }
             }
             ingredients.setText(IngredientList);
-            */
-
-
-           /* while (resultIngredients.next()) {
-                //ingredients.setText(resultContain.getString("Number")+" ");
-                //ingredients.setText(resultContain.getString("Unit")+" ");
-                ingredients.setText(resultIngredients.getString("IngredientName")+"\n");
-            }*/
-
-            /*for(int i=0; i<IngredientList.size(); i=i+3){
-                ingredients.setText(IngredientList.get(i)+" ");
-                ingredients.setText(IngredientList.get(i+1)+" ");
-                ingredients.setText(IngredientList.get(i+2)+"\n");
-            }*/
 
             /////////////////RecipeID!!!!!!!!
             sql3 = "SELECT * FROM favor WHERE Username='"+userLogin+"' AND RecipeID= '1' ";
@@ -130,10 +127,11 @@ public class DisplayRecipe extends AppCompatActivity {
                 }
 
             stmt.close();
+            stmt1.close();
             con.close();
         }
         catch (SQLException se){
-            Toast errorToast = Toast.makeText(DisplayRecipe.this, "يجب أن تكون متصلا ًبالانترنت !!!!!!"+se.getMessage() ,Toast.LENGTH_SHORT);
+            Toast errorToast = Toast.makeText(DisplayRecipe.this, "يجب أن تكون متصلا ًبالانترنت"+se.getMessage() ,Toast.LENGTH_SHORT);
             errorToast.show();
         }
         catch (Exception e){
@@ -179,7 +177,7 @@ public class DisplayRecipe extends AppCompatActivity {
            }
             else{
                 /////////////////RecipeID!!!!!!!!
-                sql1 = "DELETE FROM favor '"+userLogin+"' AND RecipeID= '1' ";
+                sql1 = "DELETE FROM favor WHERE Username='"+userLogin+"' AND RecipeID= '1' ";
                 result1 = stmt.executeUpdate(sql1);
                 numofF--;
                 if (result1 == 1) {
