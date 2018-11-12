e93866af2251836dba5af7879322ae884d665078
safaaksu <42803832+safaaksu@users.noreply.github.com>
2018-11-12 11:43:34 +0300

    Update SearchByIngredents.java

--- app/src/main/java/com/example/abodi/wshalghada/SearchByIngredents.java
+++ app/src/main/java/com/example/abodi/wshalghada/SearchByIngredents.java
@@ -1,13 +1,17 @@
 package com.example.abodi.wshalghada;
 
-import android.annotation.SuppressLint;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import android.os.StrictMode;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,127 +19,165 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.List;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class SearchByIngredents extends Fragment {
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    ResultSet resultSet = null;
 
-    private String mParam1;
-    private String mParam2;
-
-
-
-    Button btnChoice;
-    TextView TVItemSelected;
-
-
-
-
-
+    private ArrayList<String> IngCategory = new ArrayList<>();
+    private ArrayList<String[]> Ingredents = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private TextView TVItemSelected;
+    private SearchResult searchresult=new SearchResult();
+    private Button goButton;
+    private String URL=DBConnection.serverSideURL+"getcategoriesIng";
     public SearchByIngredents() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchByIngredents.
-     */
-    public static SearchByIngredents newInstance(String param1, String param2) {
-        SearchByIngredents fragment = new SearchByIngredents();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-
-
-
-
-    }
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_search_by_ingredents, container, false);
-        btnChoice =  view.findViewById(R.id.btnVege);
-        TVItemSelected= view.findViewById(R.id.tvVege);
-        btnChoice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-           // AlertDialog.Builder builder= new AlertDialog.Builder(getActivity().this);
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            String[] ingredientsArray=new String[]{"بطاطس","جزر","كوسه","باميا"};
-            final boolean[] checkedingredientsArray=new boolean[]{false,false,false,false};
-            final List<String>  ingredientsList= Arrays.asList(ingredientsArray);
-            builder.setTitle("اختر المكونات");
-            builder.setMultiChoiceItems(ingredientsArray, checkedingredientsArray, new DialogInterface.OnMultiChoiceClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-                    checkedingredientsArray [which]= isChecked;
-                    //checkedingredientsArray [which]=true;
-                    String currentItem =ingredientsList.get(which);
-                  //  Toast.makeText(SearchByIngredents.this, currentItem+"" + isChecked, Toast.LENGTH_SHORT).show();
-                    Toast.makeText(getActivity(), currentItem+"" + isChecked, Toast.LENGTH_SHORT).show();
+        View v= inflater.inflate(R.layout.fragment_search_by_ingredents, container, false);
+        TVItemSelected=v.findViewById(R.id.tvVege);
+        goButton=v.findViewById(R.id.button3);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, true);
+        recyclerView = v.findViewById(R.id.Allcategories);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(),2));
+        goButton.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            Bundle args = new Bundle();
+                                            args.putString("ItemSelected",TVItemSelected.toString());
+                                            searchresult.setArguments(args);
+                                            FragmentTransaction fr= getFragmentManager().beginTransaction();
+
+                                            fr.replace(R.id.fragment_container,searchresult).commit();
+                                        }
+                                    }
+
+        );
+
+
+       GetXMLTask task = new GetXMLTask();
+        task.execute(URL);
+        return v;
+
+    }
+
+    private class GetXMLTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute(){}
+        @Override
+        protected String doInBackground(String... urls) {
+            String output = null;
+
+            for (String url : urls) {
+                output = getOutputFromUrl(url);
+            }
+            return output;
+        }
+
+        private String getOutputFromUrl(String url) {
+            StringBuffer output = new StringBuffer("");
+            try {
+                InputStream stream = getHttpConnection(url);
+                BufferedReader buffer = new BufferedReader(
+                        new InputStreamReader(stream));
+                String s = "";
+                while ((s = buffer.readLine()) != null)
+                    output.append(s);
+
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            return output.toString();
+        }
+
+        // Makes HttpURLConnection and returns InputStream
+        private InputStream getHttpConnection(String urlString)
+                throws IOException {
+            InputStream stream = null;
+            URL url = new URL(urlString);
+            URLConnection connection = url.openConnection();
+//            String postParameters="username="+UserName.getText().toString()+"&Password="+Password.getText().toString();
+
+            try {
+                HttpURLConnection httpConnection = (HttpURLConnection) connection;
+                httpConnection.setRequestMethod("POST");
+                httpConnection.setDoOutput(true);
+                httpConnection.setDoInput(true);
+//                httpConnection.setFixedLengthStreamingMode(
+//                        postParameters.getBytes().length);
+//                PrintWriter out = new PrintWriter(httpConnection.getOutputStream());
+//                out.print(postParameters);
+//                out.close();
+                httpConnection.connect();
+
+                if (httpConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    stream = httpConnection.getInputStream();
                 }
-            });
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return stream;
+        }
 
-            builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                @SuppressLint("SetTextI18n")
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    TVItemSelected.setText("");
-                   TVItemSelected.setText("أنت اخترت هذه المكونات:"+"\n");
-                   for(int i=0; i<checkedingredientsArray.length; i++){
-                       boolean checked=checkedingredientsArray[i];
-                       if(checked){
+        @Override
+        protected void onPostExecute(String output) {
+            if(output.equals("there is no Ingredents")){Toast.makeText(getActivity(), "يوجد خطأ",
+                    Toast.LENGTH_LONG).show();}
 
-                           TVItemSelected.append(ingredientsList.get(i)+", ");
-                           //
-                       }
-                   }
+            else{
+                Ingredent[] categIng;
+                ObjectMapper objectMapper = new ObjectMapper();
+                try {
+                    categIng= (Ingredent[]) objectMapper.readValue(output, Ingredent[].class);
+                    if(categIng!=null)
+
+                        for (int j = 0; j <categIng.length ; j++) {
+                            IngCategory.add(categIng[j].getCategory());
+                            Ingredents.add(categIng[j].getIngredents());
+                        }
+RecyclerViewAdapterSearch adapter = new RecyclerViewAdapterSearch(getActivity(),TVItemSelected, IngCategory, Ingredents);
+        recyclerView.setAdapter(adapter);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            });
-
-            builder.setNeutralButton("cancel", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    //ايش راح يصير لما يحط كانسل
-                }
-            });
-
-
-            AlertDialog dialog= builder.create();
-            dialog.show();
 
 
             }
-        });
 
-
-        return view;
+        }
     }
 
 
+
+
+
 }
+
